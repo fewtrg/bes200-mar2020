ee143d52b3f2b1a8a8f5644a604e13a2fd18ea60
fewtrg <60783720+fewtrg@users.noreply.github.com>
2020-03-04 15:54:26 +0000

    angularclient

---
+++ LibraryApi/Controllers/CacheController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Controllers
+{
+    public class CacheController : Controller
+    {
+
+        [HttpGet("/time")]
+        [ResponseCache(Duration =130, Location =ResponseCacheLocation.Any)] //any is public; client is private; none = no cache
+        public ActionResult<string> GetTime()
+        {
+            return Ok(new { data = $"the time is {DateTime.Now.ToLongTimeString()}" });
+        }
+    }
+}
